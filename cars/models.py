d10aca91d6ab1d76444cf6d7dc69310f07b3d627
Vitalii Bilodid <SBSerBoss@gmail.com>
2021-03-13 23:39:23 +0200

    Добавил поле модель

--- cars/models.py
+++ cars/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Car(models.Model):
     brand = models.CharField(max_length=255)
+    model = models.CharField(max_length=100)
 
     def __str__(self):
         return self.brand
